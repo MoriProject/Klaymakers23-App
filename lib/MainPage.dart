1f48f9787ad842a63ef9949db9b3ba2329e745d4
ESblueRin <chzhdndbaoq@naver.com>
2023-10-16 23:50:49 +0900

    Creating MainPage
    Making MainPage

---
+++ lib/MainPage.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+
+class MainPage extends StatefulWidget{
+
+  const MainPage({Key? key}) : super(key: key);
+
+  @override
+  MainPageHome createState() => MainPageHome();
+}
+
+
+class MainPageHome extends State<MainPage>{
+
+  @override
+  void initState(){
+
+    super.initState();
+  }
+
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+
+
+    );
+
+  }
+
+
+
+}
